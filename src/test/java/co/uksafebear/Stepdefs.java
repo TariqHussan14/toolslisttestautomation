1f2101aff52b8f017352ecd31b17faa852ccd26a
User <student@safebear.co.uk>
2019-07-23 11:06:51 +0100

    tool Stepdefs

--- src/test/java/co/uksafebear/Stepdefs.java
+++ src/test/java/co/uksafebear/Stepdefs.java
@@ -6,11 +6,13 @@ import cucumber.api.java.en.When;
 
 import static org.junit.Assert.*;
 
-public class Stepdefs {@Given("I am logged out,")
-public void i_am_logged_out() {
-    // Write code here that turns the phrase above into concrete actions
-    throw new cucumber.api.PendingException();
-}
+public class Stepdefs {
+
+    @Given("I am logged out,")
+    public void i_am_logged_out() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new cucumber.api.PendingException();
+    }
 
     @When("I enter username {string} and Password {string}")
     public void i_enter_username_letmein(String username, String password) {
@@ -23,5 +25,24 @@ public void i_am_logged_out() {
         // Write code here that turns the phrase above into concrete actions
         throw new cucumber.api.PendingException();
     }
+
+    @Given("I am logged in,")
+    public void i_am_logged_in() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new cucumber.api.PendingException();
+    }
+
+    @When("i delete the name {string}")
+    public void i_delete_the_name(String string) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new cucumber.api.PendingException();
+    }
+
+    @Then("a message appears tool has been romved successfully")
+    public void a_message_appears_tool_has_been_romved_successfully() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new cucumber.api.PendingException();
+    }
+
 
 }
