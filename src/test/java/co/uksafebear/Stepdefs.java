d014a528d6dd71537ca7a2cf57e496092a4abc8c
User <student@safebear.co.uk>
2019-07-24 11:52:05 +0100

    website URL setup test

--- src/test/java/co/uksafebear/Stepdefs.java
+++ src/test/java/co/uksafebear/Stepdefs.java
@@ -1,12 +1,42 @@
 package co.uksafebear;
 
+import co.uksafebear.utils.Driver;
+import cucumber.api.java.After;
+import cucumber.api.java.Before;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
+import org.openqa.selenium.WebDriver;
 
 import static org.junit.Assert.*;
 
 public class Stepdefs {
+
+    WebDriver driver;
+
+    @Before
+    public void setup() {
+
+        //get the driver for the browser (e.g ChromeDriver)
+        driver = Driver.getDiver();
+        // Navigate to the URL of our webpage
+        driver.get(Driver.getURl());
+    }
+
+
+     @After
+     public void tearDown(){
+
+        //no code!!!!!
+         try {
+             Thread.sleep(Integer.parseInt(System.getProperty("sleep", "2000")));
+         } catch (InterruptedException e) {
+             e.printStackTrace();
+         }
+
+         driver.quit();
+     }
+
 
     @Given("I am logged out,")
     public void i_am_logged_out() {
